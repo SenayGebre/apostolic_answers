ac8875e1ad5124de4cbb85a1d70f3ab68f2ee539
Senay Gebre <sg.senay@gmail.com>
2025-03-09 08:04:10 +0300

    project setup done

---
+++ bricks/domain_layer/__brick__/_application.dart
@@ -0,0 +1,17 @@
+/*
+|--------------------------------------------------------------------------
+| File Information
+|--------------------------------------------------------------------------
+| File: _application.dart
+| Author: {{author_name.titleCase()}}
+| Created: {{created_year}}-{{created_month}}-{{created_day}}
+| Description: File for feature {{name.titleCase()}}
+|
+| Copyright © {{created_year}} {{author_name}}.
+| This code is licensed under the MIT License unless otherwise stated.
+| Unauthorized use, sharing, or duplication is prohibited.
+|--------------------------------------------------------------------------
+*/
+
+export 'providers/_providers.dart';
+export 'routing/_routing.dart';
